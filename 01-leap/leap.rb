996c2c8741cf988774348cb1c38a04445082c200
sai samant <saisamant@gmail.com>
2017-02-18 07:05:41 -0800

    code passes tests

---
+++ 01-leap/leap.rb
@@ -0,0 +1,16 @@
+def leap_year? year
+  if year % 4 == 0
+    if year % 100 == 0
+      if year % 400 == 0
+        true
+      else
+        false
+      end
+    else
+      true
+    end
+  else
+    false
+  end
+
+end
