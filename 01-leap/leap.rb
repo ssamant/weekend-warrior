4545b2a4660943b2d968d2cdcace49ced15b5754
sai samant <saisamant@gmail.com>
2017-02-21 10:27:11 -0800

    added alternative solution

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,16 +1,19 @@
-def leap_year? year
-  if year % 4 == 0
-    if year % 100 == 0
-      if year % 400 == 0
-        true
-      else
-        false
-      end
-    else
-      true
-    end
-  else
-    false
-  end
+# def leap_year? year
+#   if year % 4 == 0
+#     if year % 100 == 0
+#       if year % 400 == 0
+#         true
+#       else
+#         false
+#       end
+#     else
+#       true
+#     end
+#   else
+#     false
+#   end
+# end
 
+def leap_year? year
+  return ( year % 4 == 0  && ( year % 100 != 0 || year % 400 == 0))
 end
